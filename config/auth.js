f9351d6d88d9988244bfac0f477c2b2c4b5d05ce
sfreeman422 <sfreeman422@gmail.com>
2017-01-28 07:05:46 -0500

    Updated auth.js to include the heroku site

--- config/auth.js
+++ config/auth.js
@@ -4,16 +4,16 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '773053662846765', //clientID provided by FB
 		'clientSecret': '977e9e5329c0273819230a0d301ae957', //client secret provided by FB
-		'callbackURL': 'http://localhost:3000/auth/facebook/callback' //THIS WILL NEED TO BE CHANGED IN PROD
+		'callbackURL': 'https://mighty-spire-54409.herokuapp.com/auth/facebook/callback' //THIS WILL NEED TO BE CHANGED IN PROD
 	},
 	'twitterAuth':{
 		'consumerKey': '0BoEjUsl7tfCJI7plfkFMlIs9',
 		'consumerSecret': 'Pa5ZtWF1Kf1gS4d1h9I8k4xG8fXEcoeUVtbyoyC9X2j1kQTmVF',
-		'callbackURL': 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL': 'https://mighty-spire-54409.herokuapp.com/auth/twitter/callback'
 	},
 	'googleAuth': {
 		'clientID' : '',
 		'clientSecret': '',
-		'callbackURL': 'http://localhost:3000/auth/google/callback'
+		'callbackURL': 'https://mighty-spire-54409.herokuapp.com/auth/google/callback'
 	}
 };
